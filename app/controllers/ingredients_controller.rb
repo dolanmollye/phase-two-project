39f71c53aa18cc9e0e0496c3b9ca4463f18682e5
Molly <dolanmollye@gmail.com>
2021-01-05 17:05:00 -0800

    updates

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,2 +1,5 @@
 class IngredientsController < ApplicationController
+    def index
+        @ingredients = ingredients.all
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root "user#index"
   get "/user", to: "user#index"
-  
+  resources :ingredients, only: [:index, :show]
 end
