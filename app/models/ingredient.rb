b0d6af59407c53d0c39250c67289eb7dc14fc41c
Molly <dolanmollye@gmail.com>
2021-01-08 01:49:09 -0800

    added initial CSS styles

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -2,6 +2,6 @@ class Ingredient < ApplicationRecord
     has_many :pantry_ingredients
     has_many :pantries, through: :pantry_ingredients
 
-    validates :quantity, numericality: {greater_than: 0, message: "%{value} is not a numerical value"}
+    validates :quantity, numericality: {greater_than: 0, message: "Please enter %{value} in numeric form"}
 
 end
